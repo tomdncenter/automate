65f941c2c274e0215c9f25cb811249912a32d5f1
tom.dncenter <tom.dncenter@gmail.com>
2025-09-19 14:03:37 +0700

    struct

--- src/services/logger.py
+++ src/services/logger.py
@@ -3,7 +3,7 @@ import logging
 # Configure logging
 def configure_logging(name: str) -> logging.Logger:
     logging.basicConfig(
-        filename=name,          # log file name
+        filename=f"/var/log/automate/{name}",          # log file name
         filemode="a",                # "a" = append, "w" = overwrite
         format="%(asctime)s - %(levelname)s - %(message)s",
         level=logging.INFO           # log level: DEBUG, INFO, WARNING, ERROR, CRITICAL
